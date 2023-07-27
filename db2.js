a1b005bad9bf624d4ebe9bcd385f18b8d1d2d8cd
Alina Gattermann <gattermannalina@gmail.com>
2023-07-27 17:51:52 +0200

    connection and routes for photographers to postgresql

--- db2.js
+++ db2.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 // const connectionString = process.env.CONNECTION_STRING
-const connectionString = 'postgres://hxquillz:nH9ACwqrbnkha0ha0RzGr5014JEfrkS8@trumpet.db.elephantsql.com/hxquillz'
+const connectionString = 'postgres://dnkuyocl:HwIhgqkGkucBJtmwmD0pf7FFI9xFtDKo@trumpet.db.elephantsql.com/dnkuyocl'
 
 const pool = new Pool({
     connectionString,
